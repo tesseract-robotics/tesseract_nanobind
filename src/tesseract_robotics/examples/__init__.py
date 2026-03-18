655de2bf024f43af72067249142bfbaeae2b12d1
Michael Ripperger <michael.ripperger@swri.org>
2026-03-18 18:06:58 -0500

    Updated example modules __init__.py to export run and main functions

--- src/tesseract_robotics/examples/__init__.py
+++ src/tesseract_robotics/examples/__init__.py
@@ -1,1 +1,21 @@
 # Examples package
+from .abb_irb2400_viewer import main as abb_irb2400_viewer
+from .basic_cartesian_example import run as basic_cartesian_example
+from .car_seat_example import run as car_seat_example
+from .chain_example import run as chain_example
+from .freespace_hybrid_example import run as freespace_hybrid_example
+from .freespace_ompl_example import run as freespace_ompl_example
+from .geometry_showcase_example import run as geometry_showcase_example
+from .glass_upright_example import run as glass_upright_example
+from .online_planning_example import run as online_planning_example
+from .online_planning_sqp_example import run as online_planning_sqp_example
+from .pick_and_place_example import run as pick_and_place_example
+from .puzzle_piece_auxillary_axes_example import run as puzzle_piece_auxillary_axes_example
+from .raster_example import run as raster_example
+from .scene_graph_example import run as scene_graph_example
+from .shapes_viewer import main as shapes_viewer
+from .tesseract_collision_example import main as tesseract_collision_example
+from .tesseract_kinematics_example import main as tesseract_kinematics_example
+from .tesseract_material_mesh_viewer import main as tesseract_material_mesh_viewer
+# from .twc_workcell_positioner_viewer import run as twc_workcell_positioner_viewer
+from .lowlevel import *
