e2bf837c61b307157eb8cc3313e34f772c7d1b27
Joel Kang <joel@maihem.com>
2026-04-24 17:14:29 +0100

    feat(SimplePlanner): bind profiles

--- src/tesseract_motion_planners_simple/tesseract_motion_planners_simple_bindings.cpp
+++ src/tesseract_motion_planners_simple/tesseract_motion_planners_simple_bindings.cpp
@@ -11,6 +11,18 @@
 // tesseract_motion_planners simple
 #include <tesseract_motion_planners/simple/interpolation.h>
 #include <tesseract_motion_planners/simple/simple_motion_planner.h>
+#include <tesseract_motion_planners/simple/profile/simple_planner_profile.h>
+#include <tesseract_motion_planners/simple/profile/simple_planner_fixed_size_move_profile.h>
+#include <tesseract_motion_planners/simple/profile/simple_planner_fixed_size_assign_move_profile.h>
+#include <tesseract_motion_planners/simple/profile/simple_planner_fixed_size_assign_no_ik_move_profile.h>
+#include <tesseract_motion_planners/simple/profile/simple_planner_lvs_move_profile.h>
+#include <tesseract_motion_planners/simple/profile/simple_planner_lvs_no_ik_move_profile.h>
+#include <tesseract_motion_planners/simple/profile/simple_planner_lvs_assign_move_profile.h>
+#include <tesseract_motion_planners/simple/profile/simple_planner_lvs_assign_no_ik_move_profile.h>
+
+// tesseract_common (Profile/ProfileDictionary moved here in 0.33)
+#include <tesseract_common/profile.h>
+#include <tesseract_common/profile_dictionary.h>
 
 // tesseract_environment
 #include <tesseract_environment/environment.h>
@@ -20,12 +32,16 @@
 
 namespace tp = tesseract_planning;
 namespace te = tesseract_environment;
+namespace tc = tesseract_common;
 
 NB_MODULE(_tesseract_motion_planners_simple, m) {
     m.doc() = "tesseract_motion_planners_simple Python bindings";
 
     // Import MotionPlanner base type for clone() return type
     nb::module_::import_("tesseract_robotics.tesseract_motion_planners._tesseract_motion_planners");
+
+    // Import Profile base from tesseract_command_language (re-exported from tesseract_common)
+    nb::module_::import_("tesseract_robotics.tesseract_command_language._tesseract_command_language");
 
     // ========== SimpleMotionPlanner ==========
     // Note: Not binding inheritance from MotionPlanner to avoid cross-module issues
@@ -54,4 +70,100 @@ NB_MODULE(_tesseract_motion_planners_simple, m) {
           "rotation_longest_valid_segment_length"_a = 5 * M_PI / 180,
           "min_steps"_a = 1,
           "Generate an interpolated program from a composite instruction");
+
+    // ========== SimplePlannerMoveProfile (abstract base) ==========
+    nb::class_<tp::SimplePlannerMoveProfile, tc::Profile>(m, "SimplePlannerMoveProfile");
+
+    // ========== SimplePlannerCompositeProfile ==========
+    nb::class_<tp::SimplePlannerCompositeProfile, tc::Profile>(m, "SimplePlannerCompositeProfile")
+        .def(nb::init<>());
+
+    // ========== SimplePlannerFixedSizeMoveProfile ==========
+    nb::class_<tp::SimplePlannerFixedSizeMoveProfile, tp::SimplePlannerMoveProfile>(m, "SimplePlannerFixedSizeMoveProfile")
+        .def(nb::init<int, int>(), "freespace_steps"_a = 10, "linear_steps"_a = 10)
+        .def_rw("freespace_steps", &tp::SimplePlannerFixedSizeMoveProfile::freespace_steps)
+        .def_rw("linear_steps", &tp::SimplePlannerFixedSizeMoveProfile::linear_steps);
+
+    // ========== SimplePlannerFixedSizeAssignMoveProfile ==========
+    nb::class_<tp::SimplePlannerFixedSizeAssignMoveProfile, tp::SimplePlannerMoveProfile>(m, "SimplePlannerFixedSizeAssignMoveProfile")
+        .def(nb::init<int, int>(), "freespace_steps"_a = 10, "linear_steps"_a = 10)
+        .def_rw("freespace_steps", &tp::SimplePlannerFixedSizeAssignMoveProfile::freespace_steps)
+        .def_rw("linear_steps", &tp::SimplePlannerFixedSizeAssignMoveProfile::linear_steps);
+
+    // ========== SimplePlannerFixedSizeAssignNoIKMoveProfile ==========
+    nb::class_<tp::SimplePlannerFixedSizeAssignNoIKMoveProfile, tp::SimplePlannerMoveProfile>(m, "SimplePlannerFixedSizeAssignNoIKMoveProfile")
+        .def(nb::init<int, int>(), "freespace_steps"_a = 10, "linear_steps"_a = 10)
+        .def_rw("freespace_steps", &tp::SimplePlannerFixedSizeAssignNoIKMoveProfile::freespace_steps)
+        .def_rw("linear_steps", &tp::SimplePlannerFixedSizeAssignNoIKMoveProfile::linear_steps);
+
+    // ========== SimplePlannerLVSMoveProfile ==========
+    nb::class_<tp::SimplePlannerLVSMoveProfile, tp::SimplePlannerMoveProfile>(m, "SimplePlannerLVSMoveProfile")
+        .def(nb::init<double, double, double, int, int>(),
+             "state_longest_valid_segment_length"_a = 5 * M_PI / 180,
+             "translation_longest_valid_segment_length"_a = 0.1,
+             "rotation_longest_valid_segment_length"_a = 5 * M_PI / 180,
+             "min_steps"_a = 1,
+             "max_steps"_a = std::numeric_limits<int>::max())
+        .def_rw("state_longest_valid_segment_length", &tp::SimplePlannerLVSMoveProfile::state_longest_valid_segment_length)
+        .def_rw("translation_longest_valid_segment_length", &tp::SimplePlannerLVSMoveProfile::translation_longest_valid_segment_length)
+        .def_rw("rotation_longest_valid_segment_length", &tp::SimplePlannerLVSMoveProfile::rotation_longest_valid_segment_length)
+        .def_rw("min_steps", &tp::SimplePlannerLVSMoveProfile::min_steps)
+        .def_rw("max_steps", &tp::SimplePlannerLVSMoveProfile::max_steps);
+
+    // ========== SimplePlannerLVSNoIKMoveProfile ==========
+    nb::class_<tp::SimplePlannerLVSNoIKMoveProfile, tp::SimplePlannerMoveProfile>(m, "SimplePlannerLVSNoIKMoveProfile")
+        .def(nb::init<double, double, double, int, int>(),
+             "state_longest_valid_segment_length"_a = 5 * M_PI / 180,
+             "translation_longest_valid_segment_length"_a = 0.1,
+             "rotation_longest_valid_segment_length"_a = 5 * M_PI / 180,
+             "min_steps"_a = 1,
+             "max_steps"_a = std::numeric_limits<int>::max())
+        .def_rw("state_longest_valid_segment_length", &tp::SimplePlannerLVSNoIKMoveProfile::state_longest_valid_segment_length)
+        .def_rw("translation_longest_valid_segment_length", &tp::SimplePlannerLVSNoIKMoveProfile::translation_longest_valid_segment_length)
+        .def_rw("rotation_longest_valid_segment_length", &tp::SimplePlannerLVSNoIKMoveProfile::rotation_longest_valid_segment_length)
+        .def_rw("min_steps", &tp::SimplePlannerLVSNoIKMoveProfile::min_steps)
+        .def_rw("max_steps", &tp::SimplePlannerLVSNoIKMoveProfile::max_steps);
+
+    // ========== SimplePlannerLVSAssignMoveProfile ==========
+    nb::class_<tp::SimplePlannerLVSAssignMoveProfile, tp::SimplePlannerMoveProfile>(m, "SimplePlannerLVSAssignMoveProfile")
+        .def(nb::init<double, double, double, int, int>(),
+             "state_longest_valid_segment_length"_a = 5 * M_PI / 180,
+             "translation_longest_valid_segment_length"_a = 0.1,
+             "rotation_longest_valid_segment_length"_a = 5 * M_PI / 180,
+             "min_steps"_a = 1,
+             "max_steps"_a = std::numeric_limits<int>::max())
+        .def_rw("state_longest_valid_segment_length", &tp::SimplePlannerLVSAssignMoveProfile::state_longest_valid_segment_length)
+        .def_rw("translation_longest_valid_segment_length", &tp::SimplePlannerLVSAssignMoveProfile::translation_longest_valid_segment_length)
+        .def_rw("rotation_longest_valid_segment_length", &tp::SimplePlannerLVSAssignMoveProfile::rotation_longest_valid_segment_length)
+        .def_rw("min_steps", &tp::SimplePlannerLVSAssignMoveProfile::min_steps)
+        .def_rw("max_steps", &tp::SimplePlannerLVSAssignMoveProfile::max_steps);
+
+    // ========== SimplePlannerLVSAssignNoIKMoveProfile ==========
+    nb::class_<tp::SimplePlannerLVSAssignNoIKMoveProfile, tp::SimplePlannerMoveProfile>(m, "SimplePlannerLVSAssignNoIKMoveProfile")
+        .def(nb::init<double, double, double, int, int>(),
+             "state_longest_valid_segment_length"_a = 5 * M_PI / 180,
+             "translation_longest_valid_segment_length"_a = 0.1,
+             "rotation_longest_valid_segment_length"_a = 5 * M_PI / 180,
+             "min_steps"_a = 1,
+             "max_steps"_a = std::numeric_limits<int>::max())
+        .def_rw("state_longest_valid_segment_length", &tp::SimplePlannerLVSAssignNoIKMoveProfile::state_longest_valid_segment_length)
+        .def_rw("translation_longest_valid_segment_length", &tp::SimplePlannerLVSAssignNoIKMoveProfile::translation_longest_valid_segment_length)
+        .def_rw("rotation_longest_valid_segment_length", &tp::SimplePlannerLVSAssignNoIKMoveProfile::rotation_longest_valid_segment_length)
+        .def_rw("min_steps", &tp::SimplePlannerLVSAssignNoIKMoveProfile::min_steps)
+        .def_rw("max_steps", &tp::SimplePlannerLVSAssignNoIKMoveProfile::max_steps);
+
+    // ========== ProfileDictionary helpers ==========
+    m.def("ProfileDictionary_addSimplePlannerMoveProfile",
+          [](tc::ProfileDictionary& dict, const std::string& ns, const std::string& profile_name,
+             std::shared_ptr<tp::SimplePlannerMoveProfile> profile) {
+              dict.addProfile(ns, profile_name, profile);
+          }, "dict"_a, "ns"_a, "profile_name"_a, "profile"_a,
+          "Add a simple planner move profile to ProfileDictionary");
+
+    m.def("ProfileDictionary_addSimplePlannerCompositeProfile",
+          [](tc::ProfileDictionary& dict, const std::string& ns, const std::string& profile_name,
+             std::shared_ptr<tp::SimplePlannerCompositeProfile> profile) {
+              dict.addProfile(ns, profile_name, profile);
+          }, "dict"_a, "ns"_a, "profile_name"_a, "profile"_a,
+          "Add a simple planner composite profile to ProfileDictionary");
 }
